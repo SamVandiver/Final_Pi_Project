de489f3c5f47e8947ca2b0f1b454aefc7752f035
SamVandiver <samuelvandiver@gmail.com>
2022-03-25 01:07:32 -0500

    started on pygame and the code generator logic for pi 2

---
+++ piOneMain.py
@@ -0,0 +1,17 @@
+import pygame
+import random
+import socket
+import sys
+
+#   constants
+#       pi stuff
+PIHEIGHT = 800
+PIWIDTH = 600
+
+
+#   start pygame
+pygame.init()
+ 
+#   makes the screen 
+screen = pygame.display.set_mode((PIHEIGHT, PIWIDTH))
+
