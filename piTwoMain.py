731d5ae29cc1e3ef033ef2620dd1b9ef530ca2f5
Magnus Laue <mrl031@latech.edu>
2022-03-31 16:46:38 -0500

    Showed Magnus how to use Github

--- piTwoMain.py
+++ piTwoMain.py
@@ -3,3 +3,5 @@ import socket
 import sys
 import codeGenLibrary as code
 #   if you want to use a funtion in godeGenLibrary like generateCode() then do code.generateCode()
+
+print(code.generateCode(3))
