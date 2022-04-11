084dc993bbb5d529b9041a07e6a444f05eb78a32
SamVandiver <samuelvandiver@gmail.com>
2022-04-11 08:28:16 -0500

    added panels to the GUI

--- piOneMain.py
+++ piOneMain.py
@@ -1,9 +1,11 @@
+from sqlite3 import SQLITE_UPDATE
 import pygame
 import random
 import socket
 import sys
 import codeGenLibrary as CODE
 import os
+import classes
 
 '''
 # Create a connection to the server application on port 81
@@ -36,7 +38,7 @@ pygame.display.set_caption("Keep Talking")
 running = True
 
 #   change current working directory to the folder this file is in
-os.chdir("Final Pi Project/Final_Pi_Project")
+os.chdir("Final Pi Project/Final_Pi_Project/assets/buttoncolors")
 
 #   images
 #       button backgrounds
@@ -45,6 +47,7 @@ buttonWhite = pygame.image.load("buttonWhite.png")
 buttonYellow = pygame.image.load("buttonYellow.png")
 buttonRed = pygame.image.load("buttonRed.png")
 
+os.chdir("../buttonwords")
 #       button text
 abortWhite = pygame.image.load("abortWhite.png")
 
@@ -53,6 +56,12 @@ abortWhite = pygame.image.load("abortWhite.png")
 def button(buttonBackground, buttonText, x, y):
     screen.blit(buttonBackground, (x, y))
     screen.blit(buttonText, (x, y))
+
+
+#   create all the objects
+sidePanel = classes.Panel(250, PIHEIGHT, 0, 0, (60, 60, 60))
+strikePanel = classes.Panel(PIWIDTH, 200, 250, 0, (20,20,20))
+
 
 while running:
 
@@ -64,4 +73,6 @@ while running:
 
     screen.fill((100,100,100))
     button(buttonBlue, abortWhite, 450, 200)
+    sidePanel.draw()
+    strikePanel.draw()
     pygame.display.update()
